9560368f1bb5f254524408992ee89639144d337f
Maksim Loyan <loyanmaksim@gmail.com>
2018-10-22 22:29:26 +0300

    Added servlets

--- src/main/java/com/loyanix/Servlets/FileInfoServlet.java
+++ src/main/java/com/loyanix/Servlets/FileInfoServlet.java
@@ -1,4 +1,20 @@
 package com.loyanix.Servlets;
 
-public class FileInfoServlet {
+import com.loyanix.DAO.FileInfoDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FileInfoServlet extends HttpServlet {
+    protected void post(){
+
+    }
+
+    protected void get(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("fileInfo", FileInfoDao.get());
+        request.getRequestDispatcher("fileinfo.jsp").forward(request,response);
+    }
 }
--- src/main/java/com/loyanix/Servlets/LineInfoServlet.java
+++ src/main/java/com/loyanix/Servlets/LineInfoServlet.java
@@ -1,4 +1,23 @@
 package com.loyanix.Servlets;
 
-public class LineInfoServlet {
+import com.loyanix.DAO.LineInfoDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LineInfoServlet extends HttpServlet {
+    protected void post(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    protected void get(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        LineInfoDao.setId(id);
+        request.setAttribute("lineInfo", LineInfoDao.get());
+        request.getRequestDispatcher("lineinfo.jsp").forward(request,response);
+
+    }
 }
