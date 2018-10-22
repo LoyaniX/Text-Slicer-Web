e98b9cbcc154b5ade479e20a7546cb97d60173c3
Maksim Loyan <loyanmaksim@gmail.com>
2018-10-22 22:20:49 +0300

    Implement DAO layer

--- src/main/java/com/loyanix/DAO/FileInfoDao.java
+++ src/main/java/com/loyanix/DAO/FileInfoDao.java
@@ -1,4 +1,23 @@
 package com.loyanix.DAO;
 
+import com.loyanix.Beans.FileInfo;
+import com.loyanix.Hibernate.HibernateConfig;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
 public class FileInfoDao {
+    public static List<FileInfo> get(){
+        SessionFactory sessionFactory = HibernateConfig.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        List<FileInfo> fileInfos;
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(FileInfo.class);
+        fileInfos = criteria.list();
+        session.getTransaction().commit();
+        session.close();
+        return fileInfos;
+    }
 }
--- src/main/java/com/loyanix/DAO/LineInfoDao.java
+++ src/main/java/com/loyanix/DAO/LineInfoDao.java
@@ -1,4 +1,27 @@
 package com.loyanix.DAO;
 
+import com.loyanix.Beans.LineInfo;
+import com.loyanix.Hibernate.HibernateConfig;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
+
 public class LineInfoDao {
+    private static int id;
+     public static List<LineInfo> get(){
+         SessionFactory sessionFactory = HibernateConfig.getSessionFactory();
+         Session session = sessionFactory.openSession();
+         List<LineInfo> lineInfos;
+         session.beginTransaction();
+         Criteria criteria = session.createCriteria(LineInfo.class);
+         lineInfos = criteria.add(Restrictions.eq("fileId",id)).list();
+         session.getTransaction().commit();
+         session.close();
+         return lineInfos;
+     }
+
+    public static void setId(int id) { LineInfoDao.id = id; }
 }
